cff203b68b0ea4794bae5fd74fce7df5c708598a
Alessandro Souza <alessandrojsouza@gmail.com>
2021-09-28 12:18:55 -0300

    Inclusão das funcionalidade de exclusão, edição e adição de tarefas.

--- task/urls.py
+++ task/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista ),
+    path('', views.listaView ),
     path('tarefa/<int:id>', views.tarefaView),
+    path('formCadastroTarefa/', views.formTarefaView),
+    path('addTarefaView/', views.addTarefaView),
+    path('delTarefa/<int:id>', views.delTarefaView),
+    path('editTarefa/<int:id>', views.editTarefaView),
 ]
--- task/views.py
+++ task/views.py
@@ -1,12 +1,11 @@
-from django.http.response import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Tarefa
-
+from django.utils import timezone
 
 # Create your views here.
 
 # Lista todas as terefas e encaminha para página lista.html
-def lista(request):
+def listaView(request):
     tarefas = Tarefa.objects.all()
     return render (request, 'lista.html',{'tarefas':tarefas})
 
@@ -15,3 +14,40 @@ def lista(request):
 def tarefaView(request, id):
     tarefa = get_object_or_404 (Tarefa, pk=id)
     return render(request, 'tarefa.html',{'tarefa':tarefa})
+
+def formTarefaView(request):
+    return render(request, 'formCadastroTarefa.html')
+
+# Adiciona tarefa
+def addTarefaView(request):
+    # recupera informações do formulário
+    titulo = request.POST['titulo']
+    descricao = request.POST['descricao']
+    data = timezone.now()
+
+    tarefa = Tarefa(titulo=titulo,descricao=descricao, data=data)
+    tarefa.save()
+    return redirect('/')
+
+# Exclui uma tarefa
+def delTarefaView(request, id):
+    tarefa = get_object_or_404 (Tarefa, pk=id)
+    tarefa.delete()
+    return redirect('/')
+
+# Edita uma tarefa   
+def editTarefaView(request, id):
+    # recupera informações do formulário
+    tarefa = get_object_or_404 (Tarefa, pk=id)
+
+    # veifica se a solicitação é POST
+    if (request.method == 'POST'):
+        titulo = request.POST['titulo']
+        descricao = request.POST['descricao']
+        data = timezone.now()
+
+        tarefa = Tarefa(titulo=titulo,descricao=descricao, data=data)
+        tarefa.save()
+        return redirect('/')
+
+    return render(request, 'editTarefa.html',{'tarefa':tarefa})
